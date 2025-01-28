1e6423598428842b3e63c1166da37c9bcf3a7b5e
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2025-01-28 20:02:02 +0530

    Body parser added

--- app.js
+++ app.js
@@ -2,16 +2,31 @@
 
 //////////////////////////////////////// Express Way ///////////////////////////
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  console.log("In the middleware");
+  // console.log("In the middleware");
   next();
 });
 
+app.use("/add-product", (req, res, next) => {
+  // console.log("In another middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+
 app.use((req, res, next) => {
-  console.log("In another middleware");
+  // console.log("In another middleware");
   res.send("<h1>Hello from Express</h1>");
 });
 
