8bb75e29c8c09378ddce4722f4d3b52c7778be0e
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2025-02-01 20:16:16 +0530

    Sharing data between requests

--- Course Learnings/routes/admin.js
+++ Course Learnings/routes/admin.js
@@ -1,10 +1,9 @@
 const path = require('path');
-
 const express = require('express');
-
 const rootDir = require('../utils/path');
 
 const router = express.Router();
+const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
@@ -14,7 +13,10 @@ router.get('/add-product', (req, res, next) => {
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
+
