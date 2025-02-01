7d894776acba465f1f6d477e60f915072b309756
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2025-02-01 22:25:56 +0530

    pug files

--- Course Learnings/routes/shop.js
+++ Course Learnings/routes/shop.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../utils/path');
-
-const router = express.Router();
+const rootDir = require('../util/path');
 const adminData = require('./admin');
 
+const router = express.Router();
+
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
